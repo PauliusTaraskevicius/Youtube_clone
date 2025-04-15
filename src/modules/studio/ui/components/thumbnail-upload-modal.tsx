ceed5071fe6076f43cc09d3ac86a1a40a00a94e1
PauliusTaraskevicius <fixas1029@gmail.com>
2025-04-15 16:01:46 +0300

    video thumbnails

---
+++ src/modules/studio/ui/components/thumbnail-upload-modal.tsx
@@ -0,0 +1,37 @@
+import { ResponsiveModal } from "@/components/reponsive-modal";
+import { trpc } from "@/trpc/client";
+import { UploadDropzone } from "@/utils/uploadthing";
+
+interface ThumbnailUploadModalProps {
+  videoId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const ThumbnailUploadModal = ({
+  onOpenChange,
+  open,
+  videoId,
+}: ThumbnailUploadModalProps) => {
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    utils.studio.getMany.invalidate();
+    utils.studio.getOne.invalidate({ id: videoId });
+    onOpenChange(false);
+  };
+
+  return (
+    <ResponsiveModal
+      title="Upload thumbnail"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <UploadDropzone
+        endpoint="thumbnailUploader"
+        input={{ videoId }}
+        onClientUploadComplete={onUploadComplete}
+      />
+    </ResponsiveModal>
+  );
+};

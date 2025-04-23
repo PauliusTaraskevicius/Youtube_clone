e5826adee0e32fe618c2b81c6b1738a064acd347
PauliusTaraskevicius <fixas1029@gmail.com>
2025-04-23 15:11:12 +0300

    video page, video views

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,12 +2,14 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { createTRPCRouter } from "../init";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
+import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 
 // import { TRPCError } from "@trpc/server";
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   categories: categoriesRouter,
   videos: videosRouter,
+  videoViews: videoViewsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;

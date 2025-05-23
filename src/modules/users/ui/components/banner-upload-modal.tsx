6aa06d6d2c82ae833cc486313cc6033fea3f7b8c
PauliusTaraskevicius <fixas1029@gmail.com>
2025-05-23 15:49:21 +0300

    banner upload

---
+++ src/modules/users/ui/components/banner-upload-modal.tsx
@@ -0,0 +1,35 @@
+import { ResponsiveModal } from "@/components/reponsive-modal";
+import { trpc } from "@/trpc/client";
+import { UploadDropzone } from "@/utils/uploadthing";
+
+interface BannerUploadModalProps {
+  userId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const BannerUploadModal = ({
+  onOpenChange,
+  open,
+  userId,
+}: BannerUploadModalProps) => {
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    utils.users.getOne.invalidate({ id: userId });
+    onOpenChange(false);
+  };
+
+  return (
+    <ResponsiveModal
+      title="Upload a banner"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <UploadDropzone
+        endpoint="bannerUploader"
+        onClientUploadComplete={onUploadComplete}
+      />
+    </ResponsiveModal>
+  );
+};

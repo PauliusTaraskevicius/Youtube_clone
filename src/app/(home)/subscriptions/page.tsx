03b1d6eb293b72a51759bc681b9011dfe072a9b0
PauliusTaraskevicius <fixas1029@gmail.com>
2025-05-27 15:49:48 +0300

    subscriptions list

---
+++ src/app/(home)/subscriptions/page.tsx
@@ -0,0 +1,20 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { SubscriptionsView } from "@/modules/subscriptions/ui/views/subscriptions-view";
+
+import { HydrateClient, trpc } from "@/trpc/server";
+import React from "react";
+
+
+const Page = () => {
+  void trpc.subscriptions.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <SubscriptionsView />
+    </HydrateClient>
+  );
+};
+
+export default Page;
